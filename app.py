af6aeab631eb7ff0e3ea3663fe2048b553ecc7d2
Victor Yu Pan <victorpan3@sbcglobal.net>
2018-02-19 21:27:11 -0600

    Initial commit. I still have to fine tune query #3.

---
+++ app.py
@@ -0,0 +1,105 @@
+from __future__ import print_function
+
+import psycopg2,os
+
+# https://stackoverflow.com/questions/517970/how-to-clear-the-interpreter-console
+def clear_screen():
+    if os.name in ('nt','dos'):
+        os.system("cls")
+    elif os.name in ('linux','osx','posix'):
+        os.system("clear")
+    else:
+        print("\n") * 120
+
+# Executes a read on the database, and prints the results to screen
+def exec_query(query):
+    print()
+    # Connect to the db
+    db = psycopg2.connect(database="news")
+    c = db.cursor()
+    # execute select statement!
+    c.execute(query)
+    # print the results to screen
+    results = c.fetchall()
+    for r in results:
+        for i in range(len(r)):
+            print(r[i], end='')
+            if len(r) - i > 1:
+                print(" - ", end='')
+        print()
+    print()
+
+# Executes the Top 3 Articles query, printing results to screen.
+def find_top_articles():
+    # Look at SUCCESSFUL GETs only
+    query = """
+        select articles.title, count(log.id) Total from log, articles
+        where status='200 OK' and method='GET' and path like '/article/%'
+              and substring(path from 10 for 999) = articles.slug
+        group by articles.title
+        order by Total desc
+        limit 3;
+    """
+    exec_query(query)
+
+
+# Executes the Top Authors query, printing results to screen.
+# hat off to Marc at SO for concatenation in join: https://stackoverflow.com/questions/13274679/like-with-on-column-names
+def find_top_authors():
+    # we can use article slug in a like comparison with the path
+    # we look at *SUCCESSFUL* GETs for authors' articles.
+    # We return the author's name, and the number of views.
+    query = """
+        select authors.name Author, count(log.id) Number_Views from log
+        left join articles on log.path like '/article/' || articles.slug
+        join authors on articles.author = authors.id
+        where method='GET' and status='200 OK' and path like '/article/%'
+        group by authors.name
+        order by Number_Views desc;
+    """
+    exec_query(query)
+
+# Executes the "Days with >1% Web Server Errors" query, printing results to screen.
+# Here I'm using substring(): https://www.postgresql.org/docs/9.1/static/functions-string.html
+# I'm also converting timestamps to strings: https://www.postgresql.org/docs/9.1/static/functions-formatting.html
+# Finally, we need to only count values with a 404 error
+#  This can be done in many different ways: https://stackoverflow.com/questions/5396498/postgresql-sql-count-of-true-values
+#  I use some of the ideas from the post above, as well as a subselect
+def find_error_days():
+    #
+    query = """
+    select * from
+        (select
+        substring(to_char(time, 'YYYY-MM-DD') from 1 for 10) date
+        ROUND(COALESCE(100.0 * sum(case when status='404 NOT FOUND' then 1 else 0 end), 0) / count(*), 2) as pct_with_error,
+        from log
+        group by date) summary
+    where pct_with_error > 1.0
+    """
+    exec_query(query)
+
+if __name__ == "__main__":
+    done = False
+    while (not done):
+        # clear_screen()
+        print("Reporting Utilities")
+        print(" 1) Find Three Most Popular Articles of All Time")
+        print(" 2) Find Most Popular Article Authors of All Time")
+        print(" 3) Display Days Where >1% of Requests Errored Out")
+        print(" 4) Quit")
+        try:
+            my_choice = int(raw_input("Select an option: "))
+        except:
+            print("Oops. Something went wrong! Please try again")
+            continue
+        if my_choice==1:
+            #
+            find_top_articles()
+        elif my_choice==2:
+            #
+            find_top_authors()
+        elif my_choice==3:
+            #
+            find_error_days()
+        else:
+            done=True
